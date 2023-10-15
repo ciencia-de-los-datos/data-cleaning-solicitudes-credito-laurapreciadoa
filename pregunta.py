0a19abc8b23a54b0285abb78eea37068352ad07d
laurapreciadoa <lapreciadoa@unal.edu.co>
2023-10-15 07:59:23 -0500

    Entrega LAB
    Entrega completa

--- pregunta.py
+++ pregunta.py
@@ -8,13 +8,27 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
-
 def clean_data():
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df.dropna(inplace=True)
 
-    #
-    # Inserte su código aquí
-    #
+    df = df.apply(lambda x: x.astype(str)
+                  .str.replace("_", "-")
+                  .str.replace("-", " ")
+                  .str.replace("$", "")
+                  .str.replace(",", ""))
+
+    df = df.apply(lambda x: x.astype(str).str.lower().str.strip())
+
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], format='mixed', dayfirst=True)
+
+    df["monto_del_credito"] = df["monto_del_credito"].astype(float)
+
+    df.drop_duplicates(inplace=True)
 
     return df
+
+
+
+
